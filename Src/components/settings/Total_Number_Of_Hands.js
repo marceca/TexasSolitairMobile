1465974f0de8b683b05602b364b8d8d05ab15e02
Mike <mmarceca@aardvarkbrigade.com>
2019-03-28 22:57:04 -0400

    added total number of hands to settings

--- Src/components/settings/Total_Number_Of_Hands.js
+++ Src/components/settings/Total_Number_Of_Hands.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { View, Image, Text, StyleSheet, TouchableHighlight, ImageBackground, Dimensions} from 'react-native';
 import * as types from '../../state/actions/actions';
 import store from '../../state/store';
+import Num_Hands from './Num_Hands';
 
 const screednWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -23,8 +24,11 @@ class Total_Number_Of_Hands extends Component {
     return(
       <ImageBackground style={styles.backgroundImage} source={require("../../assets/settings_page/Settings_BG.png")}>
         <View style={styles.backgroundImageTopBar}>
-          <Text style={styles.header}>Total Number of Hands</Text>
-          <TouchableHighlight onPress={() => this.closeTotalNumberOfHands()}><Image source={require("../../assets/settings_page/White_X.png")} /></TouchableHighlight>
+          <View style={styles.topBar}>
+            <Text style={styles.header}>Total Number of Hands</Text>
+            <TouchableHighlight onPress={() => this.closeTotalNumberOfHands()}><Image source={require("../../assets/settings_page/White_X.png")} /></TouchableHighlight>
+          </View>
+          <Num_Hands />
         </View>
       </ImageBackground>
     )
@@ -33,26 +37,31 @@ class Total_Number_Of_Hands extends Component {
 
 const styles = StyleSheet.create({
   backgroundImage: {
-    height: screenHeight / 1.2,
-    width: screednWidth / 2.5,
+    height: screenHeight / 2,
+    width: screednWidth / 2,
     flexDirection: 'column',
     justifyContent: 'space-between',
     zIndex: 10,
     marginTop: 10,
-    marginLeft: 50
+    marginLeft: 50,
+    paddingLeft: 20,
+    paddingTop: 10
   },
   backgroundImageTopBar: {
     justifyContent: 'space-between',
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'relative',
-    paddingTop: 10,
-    paddingLeft: 20,
-    paddingRight: 20
+    flexDirection: 'column',
   },
   header: {
     color: 'white',
     fontSize: 20
+  },
+  topBar: {
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingTop: 5,
+    paddingLeft: 20,
+    paddingRight: 30
   }
 })
 
